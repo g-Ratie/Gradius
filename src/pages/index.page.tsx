64726e346fda7b6b63dddc5db10294685f6ab04e
1F10220146 <s1f102201469@iniad.org>
2023-08-17 15:32:38 +0900

    Revert "マージ分を修正"
    This reverts commit dd79758f19b9d1399144a32aefd83fa7fe838f72.

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,13 +1,27 @@
+import type { TaskModel } from '$/commonTypesWithClient/models';
 import { useAtom } from 'jotai';
+import { useEffect, useState } from 'react';
 import { Loading } from 'src/components/Loading/Loading';
 import { App } from 'src/test/test';
+import { apiClient } from 'src/utils/apiClient';
+import { returnNull } from 'src/utils/returnNull';
 import { userAtom } from '../atoms/user';
 import { BasicHeader } from './@components/BasicHeader/BasicHeader';
 
 const Home = () => {
   const [user] = useAtom(userAtom);
+  const [tasks, setTasks] = useState<TaskModel[]>();
+  const fetchTasks = async () => {
+    const tasks = await apiClient.tasks.$get().catch(returnNull);
 
-  if (!user) return <Loading visible />;
+    if (tasks !== null) setTasks(tasks);
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  if (!tasks || !user) return <Loading visible />;
 
   return (
     <>
